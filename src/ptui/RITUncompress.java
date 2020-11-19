85bc1252aae3e2221d339cd4488ebad4f078b3fc
Chris Weingarten <cjw7224@g.rit.edu>
2020-11-18 21:49:51 -0500

    WIP of the uncompresser.

--- src/ptui/RITUncompress.java
+++ src/ptui/RITUncompress.java
@@ -1,10 +1,51 @@
 package ptui;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RITUncompress {
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Usage: java RITUncompress compressed.rit uncompressed.txt");
-            return;
-        }
-    }
+	public static void main(String[] args) {
+
+		if (args.length != 2) {
+			// If the command line does not have two arguments, display a usage error and
+			// exit.
+			System.err.println("Usage: java RITUncompress compressed.rit uncompressed.txt");
+			System.exit(-1);
+			;
+		}
+		List<Integer> vals = parseInput(args);
+
+	}
+
+	private static List<Integer> parseInput(String[] args) {
+		File in = new File(args[0]);
+		List<Integer> ints = new ArrayList<>();
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(in));
+
+			while (br.ready()) {
+				ints.add(Integer.parseInt(br.readLine()));
+			}
+			br.close();
+			System.out.println();
+		} catch (NumberFormatException ex) {
+
+			System.err.println("The input file contains invalid characters.");
+			System.exit(-4);
+
+		} catch (IOException ex) {
+			// If the input file does not exist or is not readable, display an error message
+			// and exit.
+			System.err.println("The input file does not exist or is not readable.");
+			System.exit(-2);
+		}
+
+		return ints;
+	}
+
 }
